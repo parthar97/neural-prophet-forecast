14a3453f83ecd5cf788cb7037e28f0e2a88df05b
parthar97 <68215809+parthar97@users.noreply.github.com>
2022-11-14 17:27:22 -0600

    Update streamlit_forecast.py

--- streamlit_forecast.py
+++ streamlit_forecast.py
@@ -18,6 +18,10 @@ try:
 
     df=read_file(uploaded_file)
 
+    daily_seasonality_btn = st.sidebar.select_slider('Daily Seasonality',options=[True, False],value=False)
+    weekly_seasonality_btn = st.sidebar.select_slider('Weekly Seasonality',options=[True, False],value=True)
+    yearly_seasonality_btn = st.sidebar.select_slider('Yearly Seasonality',options=[True, False],value=True)
+
     with st.expander("Select Date & Observed Value",expanded=True):
         c1, c2 = st.columns((1, 1))
         x=c1.selectbox('Date',df.columns)
@@ -28,10 +32,10 @@ try:
         c3, c4 = st.columns((1, 1))
         events1=c3.text_input(label='Event 1 Name',value='New Year Eve')
         eventd1=c3.date_input(label='Event 1 Date Range: ',value=(dt(year=1990, month=1, day=1), 
-                            dt(year=2023, month=1, day=30)),)
+                            dt(year=2030, month=1, day=30)),)
         events2=c4.text_input(label='Event 2 Name',value='Christmas')
         eventd2=c4.date_input(label='Event 2 Date Range: ',value=(dt(year=1990, month=1, day=1), 
-                            dt(year=2023, month=1, day=30)),)
+                            dt(year=2030, month=1, day=30)),)
 
     with st.expander("Select the Lower & Upper Window for the Events & Seasonality Factor"):
         c5, c6, c7 = st.columns((1, 1, 1))
@@ -52,11 +56,9 @@ try:
 
     if rmp=='y':
         set_random_seed(40)
-        m = NeuralProphet(n_changepoints=150,daily_seasonality=True,weekly_seasonality=True,yearly_seasonality=True,num_hidden_layers=1,loss_func='MAE')
+        m = NeuralProphet(n_changepoints=10,daily_seasonality=daily_seasonality_btn,weekly_seasonality=weekly_seasonality_btn,yearly_seasonality=yearly_seasonality_btn,num_hidden_layers=1,loss_func='MAE',epochs=5,)
         event1 = pd.DataFrame({'event': events1,'ds': pd.to_datetime(eventd1).date})
         event2 = pd.DataFrame({'event': events2,'ds': pd.to_datetime(eventd2).date})
-        # event1 = pd.DataFrame({'event': events1,'ds': eventd1})
-        # event2 = pd.DataFrame({'event': events2,'ds': eventd2})
         enames=[events1,events2]
         events_df = pd.concat([event1,event2])
 
@@ -65,7 +67,7 @@ try:
             if enames[i]!='':
                 m=m.add_events([enames[i]],lower_window=lw,upper_window=uw,mode=mode)
         history_df = m.create_df_with_events(df, events_df)
-        metrics = m.fit(history_df, freq=freq)
+        metrics = m.fit(history_df, freq=freq,)
         future = m.make_future_dataframe(df=history_df, events_df=events_df, periods=periods)
         forecast = m.predict(df=future) 
         fig = m.plot(forecast)
@@ -76,49 +78,20 @@ try:
         st.pyplot(fig_param)
         st.dataframe(forecast)
 
+        @st.experimental_memo
         def convert_df(df):
-            return df.to_csv().encode('utf-8')
+            return df.to_csv(index=False).encode('utf-8')
 
-        forecast_df = convert_df(forecast)
-        
         try:
+            forecast_df = convert_df(forecast)
             if forecast_df is not None:
                 st.download_button(label="Download data as CSV",data=forecast_df,file_name='NeuralProphet_with_events_results.csv',mime='text/csv',)
         except:
             st.write('Choose Something')
+        
 except:
     st.write('Choose Something')
 
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
-st.sidebar.write('')
 st.sidebar.write('### **About**')
 st.sidebar.info(
  """
